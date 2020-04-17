428ab7fd06126784020d91619b9faa8dc98323e4
suzonraj <suzonraj@ymail.com>
2020-04-17 12:18:24 +0600

    added: base layout html

--- PyGithub/urls.py
+++ PyGithub/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+
     path('admin/', admin.site.urls),
+
+    path('auth/', include('django.contrib.auth.urls')),
+    path('signup', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
